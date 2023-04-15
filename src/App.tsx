e26e263d7eecda771d4cceb19d167bb0e8422aad
Hojjat Afsharan <afsharan.h@gmail.com>
2023-04-15 13:04:45 +0200

    add undo and redo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,37 @@ type Point = {
 function App() {
 
   const [points, setPoints] = useState<Point[]>([]);
+  const [undoPoints, setUndoPoints] = useState<Point[]>([]);
   
 
   function handleMouseClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>): void {
     setPoints([...points, {x: event.clientX, y: event.clientY}]);
   }
 
+  function handleUndo(): void {
+    let newPoints = [...points];
+    let undoPoint = newPoints.pop();
+    if(!undoPoint) return;
+    setUndoPoints([...undoPoints, undoPoint]);
+    setPoints(newPoints);
+  }
+
+  function handleRedo(): void {
+    let undo = [...undoPoints];
+    let lastUndoPoint = undo.pop();
+    setUndoPoints([...undo]);
+    if(!lastUndoPoint) return;
+    setPoints([...points, lastUndoPoint]);
+  }
+
   return (
     <div className="App">
+        <button className='undo' onClick={handleUndo}>Undo</button>
+        <button className='redo' onClick={handleRedo}>Redo</button>
       <div className="container" onClick={handleMouseClick}>
+    
         {points.map((point, index) => {
-          return <div key={index} className="point" style={{left: point.x, top: point.y}}></div>
+          return <div key={index} className="point" style={{left: point.x - 6 +'px', top: point.y - 6 + 'px'}}></div>
         })}
       </div>
     </div>
